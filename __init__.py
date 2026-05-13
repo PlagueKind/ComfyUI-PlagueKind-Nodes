3a531a846453d8aeef42fe9bc1af0531304d812a
PlagueKind <tombthread@gmail.com>
2026-05-13 23:58:05 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .unified_resize.unified_resize import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-WEB_DIRECTORY = "./unified_resize/js"
-
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
