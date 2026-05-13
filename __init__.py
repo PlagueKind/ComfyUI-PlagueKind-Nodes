244d487d81de3f1acfb535fa33c81f550afee0ef
PlagueKind <tombthread@gmail.com>
2026-05-13 23:59:05 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .unified_resize.unified_resize import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+WEB_DIRECTORY = "./unified_resize/js"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
